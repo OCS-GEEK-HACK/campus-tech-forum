f8e30f2231b4ef7918d0fc7e48bf2abefbd11dc2
yuto ichinose <yuto.0526.sano@gmail.com>
2024-12-14 21:13:39 +0900

    fix: エスケープ処理追加

--- php/www/components/article-card/element.php
+++ php/www/components/article-card/element.php
@@ -1,8 +1,8 @@
 <div class="col-md-4 mb-3">
     <div class="card border rounded p-3">
-        <h5 class="mb-2"><?= $title ?></h5>
-        <p class="text-muted"><?= $date ?></p>
-        <p><?= $content ?></p>
-        <a href="<?= $link ?>" class="btn btn-dark btn-sm">詳細を見る</a>
+        <h5 class="mb-2"><?= htmlspecialchars($title) ?></h5>
+        <p class="text-muted"><?= htmlspecialchars($date) ?></p>
+        <p><?= htmlspecialchars($content) ?></p>
+        <a href="<?= htmlspecialchars($link) ?>" class="btn btn-dark btn-sm">詳細を見る</a>
     </div>
 </div>
