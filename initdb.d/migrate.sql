45edb31d42730616e5bb40592f2db2e354869a73
syun17 <shunki.s777@gmail.com>
2024-12-20 14:47:37 +0900

    サインアップの実装とsqlの修正

--- initdb.d/migrate.sql
+++ initdb.d/migrate.sql
@@ -8,11 +8,12 @@ CREATE TABLE users (
     email VARCHAR(255) UNIQUE NOT NULL, -- email: メアド (一意制約)
     password VARCHAR(255) NOT NULL,    -- password: ハッシュ化したパスワード
     bio TEXT,                         -- bio: 自己紹介や説明
-    image TEXT                        -- image: 画像のパス・base64
+    image TEXT,                       -- image: 画像のパス・base64
+    link VARCHAR(255)                 --link:GithubやXなどのリンク
 );
 
-INSERT INTO users (name, displayName, bio, image, email, password)
+INSERT INTO users (name, displayName, bio, image, email, password, link)
 VALUES
-('山田太郎', 'Taro Yamada', '日本のエンジニア。プログラミングが得意です。', 'path/to/image1.jpg', 'taro.yamada@example.com', crypt('password123', gen_salt('bf'))),
-('鈴木花子', 'Hanako Suzuki', 'ウェブデザイナー。美しいデザインを作成します。', 'path/to/image2.jpg', 'hanako.suzuki@example.com', crypt('mypassword', gen_salt('bf'))),
-('佐藤一郎', 'Ichiro Sato', 'システムアーキテクト。技術的な問題を解決するのが得意です。', 'path/to/image3.jpg', 'ichiro.sato@example.com', crypt('securepass', gen_salt('bf')));
+('山田太郎', 'Taro Yamada', '日本のエンジニア。プログラミングが得意です。', 'path/to/image1.jpg', 'taro.yamada@example.com', crypt('password123', gen_salt('bf')),'github.com'),
+('鈴木花子', 'Hanako Suzuki', 'ウェブデザイナー。美しいデザインを作成します。', 'path/to/image2.jpg', 'hanako.suzuki@example.com', crypt('mypassword', gen_salt('bf')), 'github.com'),
+('佐藤一郎', 'Ichiro Sato', 'システムアーキテクト。技術的な問題を解決するのが得意です。', 'path/to/image3.jpg', 'ichiro.sato@example.com', crypt('securepass', gen_salt('bf')), 'github.com');
